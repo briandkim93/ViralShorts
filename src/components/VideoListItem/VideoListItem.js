9a5dfc1ad152a8be556c55bfde0ab0b1f62cd025
Brian Kim <briandkim93@gmail.com>
2018-06-27 11:34:10 -0700

    getViewCount(video) defined for use in fetching YouTube video view count statistics.

--- src/components/VideoListItem/VideoListItem.js
+++ src/components/VideoListItem/VideoListItem.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import { GOOGLE_API_KEY } from '../api-key.js';
 
-const VideoItem = props => {
+class VideoItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videoViewCount: 0};
+    this.getViewCount(this.props.video);
+  }
+  getViewCount(video) {
+    async function fetchData() {
+      let response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${video.id.videoId}&key=${GOOGLE_API_KEY}`);
+      let data = await response.json();
+      return data;
+    }
+    fetchData().then((response => {
+      const viewCount = parseInt(response.items[0].statistics.viewCount, 0);
+      this.setState({videoViewCount: viewCount});
+    }));
+  }
+  render() {
     return (
-      <li className="video-item" onClick={() => {props.onSelectVideo(props.video)}}>
-        <div className="video-thumbnail">
+      <li className="video-list-item" onClick={() => {this.props.onSelectVideo(this.props.video)}}>
+        <div>
           <img 
-            src={props.video.snippet.thumbnails.default.url}
+            src={this.props.video.snippet.thumbnails.default.url}
             alt='Video Thumbnail' />
-        </div>
-        <div className="video-title">
-          {props.video.snippet.title}
+          <div>
+            <h4>{this.props.video.snippet.title}</h4>
+            <h5>{this.props.video.snippet.channelTitle}</h5>
+            <h6>View Count: {this.state.videoViewCount.toLocaleString()}</h6>
+          </div>
         </div>
       </li>
     );
+  }
 };
 
 export default VideoItem;
