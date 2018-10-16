f6f569e83b58e9e058ffb127db5474f9b82e3902
Brian Kim <briandkim93@gmail.com>
2018-10-15 17:13:21 -0700

    Add border around search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,15 +4,15 @@ import logo from './images/logo.png';
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-
     this.state = {query: ''};
   }
+  
   render() {
     return (
       <div className="search-bar my-3 media">
         <a href="."><img className="align-self-center mr-2" src={logo} alt="ViralShorts Logo" /></a>
         <input
-          className="col-12 col-xl-7 media-body"
+          className="col-12 col-xl-7 media-body border border-info"
           type="text" 
           placeholder="Search YouTube" 
           value={this.state.query}
